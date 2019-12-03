7e51340a08604b6f67d52ed4f18a74fd7a57377a
Dennis Sauve <dengsauve@yahoo.com>
2019-12-02 22:56:15 -0800

    Day 3 refactored and commented

--- day-03/main.rb
+++ day-03/main.rb
@@ -1,102 +1,18 @@
 require_relative '../lib/aoc_lib.rb'
 
 input = open("input.txt").read().split("\n")
-# puts input.inspect
+
+# 610 steps
 # input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83".split("\n")
+# 410 steps
 # input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split("\n")
-#input = "R8,U5,L5,D3\nU7,R6,D4,L4".split("\n")
+
 input.each_with_index do | cmd, i |
     input[i] = cmd.split(',')
 end
 
-# puts input.inspect
-
-line_1 = []
-line_2 = []
-
-input.each_with_index do | cmdset, i |
-    x = 0
-    y = 0
-
-    cmdset.each do | cmd |
-        instruction = cmd[0]
-        distance = cmd[1..-1].to_i
-        
-        distance.times do
-            case instruction
-            when "R"
-                x += 1
-            when "L"
-                x -= 1
-            when "U"
-                y += 1
-            when "D"
-                y -= 1
-            end
-
-            line_1.push([x,y]) if i == 0
-            line_2.push([x,y]) if i == 1
-        end
-        
-    end
-end
-
-# get the intersections
-intersections = line_1 & line_2
-
-#puts (results).inspect
-
-#sorted = results.sort_by{|e| e[0].abs + e[1].abs}
-# puts sorted.inspect
-# puts sorted[0][0].abs + sorted[0][1].abs
-
-l1r = []
-l2r = []
-
-input.each_with_index do | cmdset, i |
-    x = 0
-    y = 0
-    steps = 0
-
-    cmdset.each do | cmd |
-        instruction = cmd[0]
-        distance = cmd[1..-1].to_i
-        
-        distance.times do
-            case instruction
-            when "R"
-                x += 1
-            when "L"
-                x -= 1
-            when "U"
-                y += 1
-            when "D"
-                y -= 1
-            end
-
-            steps += 1
-
-            if i == 0
-                l1r.push([x,y,steps]) if (intersections.include?([x,y]))
-            else
-                l2r.push([x,y,steps]) if (intersections.include?([x,y]))
-            end
-        end
-        
-    end
-end
-
-#puts results.inspect
-
-combined = []
-l1r.each do |r|
-    l2r.each do |rr|
-        if r[0] == rr[0] && r[1] == rr[1]
-            combined.push([r[0], r[1], r[2] + rr[2]])
-        end
-    end
-end
-
-combined = combined.sort_by{|x| x[2]}
-
-puts combined[0..10].inspect
+mdf = ManhattanDistanceFinder.new(input)
+# Should be 1064
+puts mdf.solve()
+# Should be 25676
+puts mdf.solveFewestSteps()
--- lib/aoc_lib.rb
+++ lib/aoc_lib.rb
@@ -40,7 +40,6 @@ class FuelCalculator
 end
 
 
-
 ##
 # IntComp is a computer that parses a comma separated string of integers and
 #   parses it into commands via the "solve" method.
@@ -91,5 +90,100 @@ class IntComp
 
         return program[0]
     end
+end
 
+
+##
+# Manhattan Distance finder solves for the intersections of two lines relative
+#   to their shared origin.
+class ManhattanDistanceFinder
+
+    ##
+    # Params:
+    #   cmdset is an array of strings matching "[UDLR]\d+"
+    def initialize(cmdset)
+        @cmdset = cmdset
+    end
+
+    ##
+    # Solves the intersection closest to the origin by "manhattan" distance
+    def solve()
+        intersections = get_intersections()
+        sorted = intersections.sort_by{|e| e[0].abs + e[1].abs}
+        return sorted[0][0].abs + sorted[0][1].abs
+    end
+
+    ##
+    # Solves the intersection closest to the origin by stepping distance
+    def solveFewestSteps()
+        intersections = get_intersections()
+        l1r, l2r = get_intersections(intersections)
+
+        combined = []
+        l1r.each do |r|
+            l2r.each do |rr|
+                if r[0] == rr[0] && r[1] == rr[1]
+                    combined.push([r[0], r[1], r[2] + rr[2]])
+                end
+            end
+        end
+
+        combined = combined.sort_by{|x| x[2]}
+
+        return combined[0][2]
+    end
+
+    ##
+    # Params
+    #   intersections(optional): Intersections is a list of points. If this is
+    #       passed, it triggers the return of two separate lists with steps tracked
+    def get_intersections(intersections=nil)
+        return_steps = intersections != nil
+        
+        line_1 = []
+        line_2 = []
+
+        @cmdset.each_with_index do | cmdset, i |
+            x = 0
+            y = 0
+            steps = 0
+
+            cmdset.each do | cmd |
+                instruction = cmd[0]
+                distance = cmd[1..-1].to_i
+                
+                distance.times do
+                    case instruction
+                    when "R"
+                        x += 1
+                    when "L"
+                        x -= 1
+                    when "U"
+                        y += 1
+                    when "D"
+                        y -= 1
+                    end
+
+                    steps += 1
+                    
+                    if(return_steps)
+                        if intersections.include?([x,y])
+                            line_1.push([x,y,steps]) if i == 0
+                            line_2.push([x,y,steps]) if i == 1
+                        end
+                    else
+                        line_1.push([x,y]) if i == 0
+                        line_2.push([x,y]) if i == 1
+                    end
+                end
+            end
+        end
+
+        if(return_steps)
+            return line_1, line_2
+        else
+            intersections = line_1 & line_2
+            return intersections
+        end
+    end    
 end
