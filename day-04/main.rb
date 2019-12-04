dd0e39dde6d09f231c20788a3b13bdc386dd1db4
Dennis Sauve <dengsauve@yahoo.com>
2019-12-03 22:40:03 -0800

    Refactored Day-04

--- day-04/main.rb
+++ day-04/main.rb
@@ -1,57 +1,41 @@
-require_relative '../lib/aoc_lib.rb'
-
-lb = 246515
-ub = 739105
-
-count = 0
-
 def doubles_check(arr)
-    tstring = arr.join()
-    pass = false
+    string = arr.join()
     arr.each_with_index do |a,i|
-        if tstring.match(/[^#{a}](#{a}){2}[^#{a}]/) || 
-            tstring.match(/^(#{a}){2}[^#{a}]/) ||
-            tstring.match(/[^#{a}](#{a}){2}$/)
-            pass = true
+        if string.match(/[^#{a}](#{a}){2}[^#{a}]/) || 
+            string.match(/^(#{a}){2}[^#{a}]/) ||
+            string.match(/[^#{a}](#{a}){2}$/)
+            return true
         end
     end
-    return pass
+    return false
 end
 
-(lb..ub).each do |target|
-#[111222].each do |target|
+lower_bound = 246515
+upper_bound = 739105
+count = 0
+
+(lower_bound..upper_bound).each do |target|
     passing = true
-    prev = nil
+    previous_number = nil
     target = target.to_s.split('')
 
     target.each_with_index do |t,i|
         if i == 0
-            prev = t
+            previous_number = t
         else
-            if t < prev
+            if t < previous_number
                 passing = false
                 break
             else
-                prev = t
+                previous_number = t
             end
         end
     end
 
     passing = doubles_check(target) if passing == true
 
-    if passing == true
-        count += 1
-    end
+    count += 1 if passing == true
 end
 
 puts count
-
-#p1 1057 not right
-#p1+2 137 not right
-# 1048 right for first part, second should be less
-# 301 not right
-# 379 not right
-# 409 not right
-# 547 not correct
-# 807 not right
-# 892 not right
+# 677 for part 2
