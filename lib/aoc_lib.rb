4f870ca835d9e4986442a84c1e3270411c02fdc7
Dennis Sauve <dengsauve@yahoo.com>
2019-12-05 17:30:35 -0800

    Solved Day05.2

--- lib/aoc_lib.rb
+++ lib/aoc_lib.rb
@@ -54,6 +54,10 @@ class IntComp
         @MULTIPLY = 2
         @STORE = 3
         @READ = 4
+        @JUMP_IF_TRUE = 5
+        @JUMP_IF_FALSE = 6
+        @LESS = 7
+        @EQUALS = 8
         @STOP = 99
         @POS = 0
         @IMM = 1
@@ -79,13 +83,17 @@ class IntComp
             puts "pointer: #{pointer}" if @DEBUG
             opcode = program[pointer]
 
+            # Immeidate Mode
             if(opcode.to_s.length > 2)
                 opcode = opcode.to_s
                 # need to parse that fancy code
                 instruction = opcode[-2..-1].to_i
                 puts "instruction: #{instruction}\nopcode: #{opcode}" if @DEBUG
                 
-                break if(instruction == @STOP)
+                if(instruction == @STOP)
+                    puts "STOP"
+                    break
+                end
                 
                 if (opcode.length == 3)
                     puts "length: 3" if @DEBUG
@@ -103,9 +111,10 @@ class IntComp
                     b = opcode[1].to_i
                     c = opcode[2].to_i
                 end
+
+                puts "a: #{a}\nb: #{b}\nc: #{c}\n" if @DEBUG
                                 
                 if(instruction == @ADD || instruction == @MULTIPLY)
-                    puts "a: #{a}\nb: #{b}\nc: #{c}\n" if @DEBUG
                     position_a = program[program[pointer + 1].to_i].to_i if c == 0
                     position_a = program[pointer + 1].to_i if c == 1
 
@@ -138,12 +147,130 @@ class IntComp
                     pointer += 2
                     puts "incrementing pointer by 2" if @DEBUG
                 end
+
+                # only two params
+                if(instruction == @JUMP_IF_TRUE)
+                    if c == 1
+                        first = pointer + 1
+                    else
+                        first = program[pointer + 1].to_i
+                    end
+
+                    if b == 1
+                        second = pointer + 2
+                    else
+                        second = program[pointer + 2].to_i
+                    end
+
+                    if @DEBUG
+                        puts "first: #{first}\nsecond: #{second}"
+                        puts "first_value: #{program[first]}\nsecond_value: #{program[second]}"
+                    end
+
+                    unless program[first].to_i == 0
+                        pointer = program[second].to_i
+                        puts "assigning pointer to #{pointer}" if @DEBUG
+                    else
+                        pointer += 3
+                        puts "incrementing pointer by 3" if @DEBUG
+                    end
+                end
+
+                # only two params
+                if(instruction == @JUMP_IF_FALSE)
+                    if c == 1
+                        first = pointer + 1
+                    else
+                        first = program[pointer + 1].to_i
+                    end
+
+                    if b == 1
+                        second = pointer + 2
+                    else
+                        second = program[pointer + 2].to_i
+                    end
+                    
+                    if @DEBUG
+                        puts "first: #{first}\nsecond: #{second}"
+                        puts "first_value: #{program[first]}\nsecond_value: #{program[second]}"
+                    end
+
+                    if program[first].to_i == 0
+                        pointer = program[second].to_i
+                        puts "assigning pointer to #{pointer}" if @DEBUG
+                    else
+                        pointer += 3
+                        puts "incrementing pointer by 3" if @DEBUG
+                    end
+                end
+
+                # four parameters
+                if(instruction == @LESS)
+                    if c == 1
+                        first = pointer + 1
+                    else
+                        first = program[pointer + 1].to_i
+                    end
+
+                    if b == 1
+                        second = pointer + 2
+                    else
+                        second = program[pointer + 2].to_i
+                    end
+
+                    if a == 1
+                        third = pointer + 3
+                    else
+                        third = program[pointer + 3].to_i
+                    end
+
+                    if(program[first].to_i < program[second].to_i)
+                        program[third] = 1
+                    else
+                        program[third] = 0
+                    end
+
+                    pointer += 4
+                end
+
+                if(instruction == @EQUALS)
+                    if c == 1
+                        first = pointer + 1
+                    else
+                        first = program[pointer + 1].to_i
+                    end
+
+                    if b == 1
+                        second = pointer + 2
+                    else
+                        second = program[pointer + 2].to_i
+                    end
+
+                    if a == 1
+                        third = pointer + 3
+                    else
+                        third = program[pointer + 3].to_i
+                    end
+
+                    if(program[first].to_i == program[second].to_i)
+                        program[third] = 1
+                    else
+                        program[third] = 0
+                    end
+
+                    pointer += 4
+                end
+            # Position Mode
             else
                 opcode = opcode.to_i
                 puts "ONLY opcode: #{opcode}" if @DEBUG
-                break if(opcode == @STOP)
+                if(opcode == @STOP)
+                    puts "STOP"
+                    break
+                end
                 
                 if(opcode == @ADD || opcode == @MULTIPLY)
+                    puts "Add or Multiply" if @DEBUG
                     position_a = program[program[pointer + 1].to_i].to_i
                     position_b = program[program[pointer + 2].to_i].to_i
                     position_c = program[pointer + 3].to_i
@@ -158,6 +285,7 @@ class IntComp
                 end
     
                 if(opcode == @STORE)
+                    puts "Store" if @DEBUG
                     print "input: "
                     number = gets.strip
                     target = program[pointer + 1].to_i
@@ -168,9 +296,76 @@ class IntComp
                 end
     
                 if(opcode == @READ)
+                    puts "Read" if @DEBUG
                     puts "\nSTATUS: #{program[program[pointer + 1].to_i]}\n"
                     pointer += 2
                     puts "incrementing pointer by 2" if @DEBUG
+                end
+
+                if(opcode == @JUMP_IF_TRUE)
+                    puts "Jump if True" if @DEBUG
+                    if @DEBUG
+                        puts "first: #{program[pointer + 1]}\nsecond: #{second}"
+                        puts "first_value: #{program[program[pointer + 1].to_i]}\nsecond_value: #{program[program[pointer + 2].to_i]}"
+                    end
+                    unless program[pointer + 1].to_i == 0
+                        pointer = program[program[pointer + 2].to_i].to_i
+                        puts "assigning pointer to #{pointer}" if @DEBUG
+                    else
+                        pointer += 3
+                        puts "incrementing pointer by 3" if @DEBUG
+                    end
+                end
+
+                if(opcode == @JUMP_IF_FALSE)
+                    puts "Jump if False" if @DEBUG
+                    if program[pointer + 1].to_i == 0
+                        pointer = program[program[pointer + 2].to_i].to_i
+                        puts "assigning pointer to #{pointer}" if @DEBUG
+                    else
+                        pointer += 3
+                        puts "incrementing pointer by 3" if @DEBUG
+                    end
+                end
+
+                if(opcode == @LESS)
+                    puts "Less than" if @DEBUG
+                    first = program[pointer + 1].to_i
+                    second = program[pointer + 2].to_i
+                    third = program[pointer + 3].to_i
+
+                    puts "first_value: #{program[first]}\nsecond_value: #{program[second]}\nthird_value: #{program[second]}" if @DEBUG
+
+                    if(program[first].to_i < program[second].to_i)
+                        puts "setting 1 to position #{third}"
+                        program[third] = 1
+                    else
+                        puts "setting 0 to position #{third}"
+                        program[third] = 0
+                    end
+
+                    pointer += 4
+                    puts "incrementing pointer by 4" if @DEBUG
+                end
+
+                if(opcode == @EQUALS)
+                    puts "Equals" if @DEBUG
+                    first = program[pointer + 1].to_i
+                    second = program[pointer + 2].to_i
+                    third = program[pointer + 3].to_i
+
+                    puts "first_value: #{program[first]}\nsecond_value: #{program[second]}\nthird_value: #{program[second]}" if @DEBUG
+
+                    if(program[first].to_i == program[second].to_i)
+                        puts "setting 1 to position #{third}"
+                        program[third] = 1
+                    else
+                        puts "setting 0 to position #{third}"
+                        program[third] = 0
+                    end
+
+                    pointer += 4
+                    puts "incrementing pointer by 4" if @DEBUG
                 end
             end
             gets if @DEBUG
