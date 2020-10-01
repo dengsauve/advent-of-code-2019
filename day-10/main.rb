75566adcfd6d6b76fb674f707d1216b22db568a0
Dennis Sauve <dengsauve@yahoo.com>
2020-09-30 20:24:47 -0700

    Update main.rb

--- day-10/main.rb
+++ day-10/main.rb
@@ -1,2 +1,26 @@
-input = open("input.txt").readlines
+# input = open("input.txt").readlines(chomp: true)
 
+test_1_input = ".#..#
+.....
+#####
+....#
+...##".split("\n")
+
+puts test_1_input.inspect
+
+class AsteroidMap
+
+  def initialize(map)
+    @map = map
+  end
+
+end
+
+class Asteroid
+
+  def initialize(position_x, position_y)
+    @position_x = position_x
+    @position_y = position_y
+  end
+
+end
