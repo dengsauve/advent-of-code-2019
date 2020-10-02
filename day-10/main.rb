ce0a94e8bdd1267d31d49855df5bee76a3e41460
Dennis Sauve <dengsauve@yahoo.com>
2020-10-01 18:39:01 -0700

    working on main

--- day-10/main.rb
+++ day-10/main.rb
@@ -10,8 +10,22 @@ puts test_1_input.inspect
 
 class AsteroidMap
 
+  attr_accessor :asteroids
+
   def initialize(map)
     @map = map
+    @asteroids = []
+    self.parse_asteroids
+  end
+
+  def parse_asteroids
+    @map.each_with_index do |row, ix|
+      row.split("").each_with_index do |col, iy|
+        if col == "#"
+          @asteroids << Asteroid.new(iy, ix)
+        end
+      end
+    end
   end
 
 end
@@ -23,4 +37,15 @@ class Asteroid
     @position_y = position_y
   end
 
+  def to_s
+    "#{@position_x}, #{@position_y}"
+  end
+
 end
+
+def test_one(input)
+  asteroid_map = AsteroidMap.new(input)
+  puts asteroid_map.asteroids
+end
+
+test_one(test_1_input)
